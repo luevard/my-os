cb3b1f350fe20b4a02959f35c04dabb88838fc4c
EVARD Lucas <lucas.evard@gck.co>
2026-03-26 15:55:07 +0100

    :sparkles: Enable protected mode and print 'A' character

--- boot.s
+++ boot.s
@@ -1,22 +1,47 @@
 [BITS 16] ; Assemble the code for 16-bit real mode
-[ORG 0x7C00] ; Conventionnal Bootloader address
+[ORG 0x7C00] ; Conventional Bootloader load address
 
 start:
-    mov bx, msg ; Load the offset address of msg into BX
-    call printmsg ; Call printmsg and save return address on the stack
-printmsg:
-    mov al, [bx] ; Save the current char into al register
-    cmp al, 0 ; Compare al register with the '0' value
-    jz halt ; If true | set the next code segment to halt function
-    mov ah, 0x0E ; BIOS teletype function for displaying one character
-    int 0x10 ; Call BIOS video interrupt to print AL
-    inc bx ; Increment bx register for point to the next char
-    jmp printmsg ; Recursive call
+    cli ; Disable interrupts
+    lgdt [gdt_descriptor] ; Load the GDT descriptor
+    mov eax, cr0 ; Save cr0 state (32 bits) into eax register
+    or eax, 0x00000001 ; Change the PE bit (bit^0) to 1 to enable protected mode 
+    mov cr0, eax ; Save the modification into the cr0 register
+    jmp CODE_SEGMENT:start_protected_mode ; jump to 32 bits instructions
 
+gdt_start: ; Describe GDT flat model
+    null_descriptor: ; Null descriptor (mandatory first GDT entry)
+        dd 0
+        dd 0
+    code_descriptor: ; Describe flags of code segment
+        dw 0xffff ; Memory limit
+        dw 0 ; Base low
+        db 0 ; Base mid
+        db 10011010b ; Access byte: P=1, DPL=00, S=1, Type=1010 (exec/read)
+        db 11001111b ; Flags: G=1, D=1, L=0, AVL=0 + Limit high=0xF
+        db 0 ; Base high
+    data_descriptor: ; Describe flags of data segment
+        dw 0xffff
+        dw 0
+        db 0
+        db 10010010b ; Access byte: P=1, DPL=00, S=1, Type=0010 (writable)
+        db 11001111b ; Flags: G=1, D=1, L=0, AVL=0 + Limit high=0xF
+        db 0
+gdt_end: ; End of the GDT
+
+gdt_descriptor: ; Describe size of GDT
+    dw gdt_end - gdt_start -1 ; guessing size of GDT
+    dd gdt_start ; Base address of the GDT
+CODE_SEGMENT equ code_descriptor - gdt_start ; Offset of the code segment descriptor in the GDT
+DATA_SEGMENT equ data_descriptor - gdt_start ; Offset of the data segment descriptor in the GDT
 halt: 
-    ret ; Return
+    jmp $ ; Infinite loop
 
-msg: db 'Hello World!', 0 ; Define "Hello World!\0" string
-
+[BITS 32] ; Assemble the code for 32-bit protected mode
+start_protected_mode: ; First 32 bits instructions executed
+    mov al, 'A' ; Load character 'A' into al register
+    mov ah, 0x0f ; Text attribute: black background, bright white foreground
+    mov [0xb8000], ax ; Write 'A' and its attribute to VGA text memory with the ax register (al || ah)
+    jmp halt ; Set the next code segment to halt function
 times 510-($-$$) db 0 ; Fill the boot sector with zeros up to 510 bytes
 dw 0xAA55 ; BIOS signature
